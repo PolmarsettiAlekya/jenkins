d9ce8b7d579fc256e3787db195c7b0f01989fccc
PolmarsettiAlekya <palekya9933992732@gmail.com>
2024-02-01 23:20:47 +0530

    Changes are made

--- app.java
+++ app.java
@@ -1,6 +1,5 @@
-class App{
-    public static void main(String args[]){
-        System.out.println("Hello world");
-        System.out.println("I am alekya");
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
     }
 }
